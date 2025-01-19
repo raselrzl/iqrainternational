223dfacf6aec6164ba1a2e87aea25c35aa643e8e
Rasel Miah <rasel6041@gmail.com>
2025-01-19 13:01:14 +0100

    form design

--- app/component/hero.js
+++ app/component/hero.js
@@ -1,5 +1,5 @@
-"use client"; 
-import Image from 'next/image';
+"use client";
+import Image from "next/image";
 
 export default function Hero() {
   return (
@@ -7,20 +7,29 @@ export default function Hero() {
       {/* Text and Button Div */}
       <div className="flex items-center justify-center w-full md:w-1/2 h-full text-white p-8">
         <div className="text-center md:text-left space-y-6">
-          <h1 className="text-3xl font-bold">IQRA International Education Consultancy</h1>
-          <p className="text-md">
-            ...provides step-by-step guidance from application 
-            to enrolment to make dreams of higher study abroad a reality for genuine students. 
-            Book your <span className="font-bold">FREE Consultation</span> with us now 
-            to find out why more than five thousand students consider us the best 
-            education consultancy in Bangladesh.
-          </p>
-          <p className="text-md">
-            Our team of expert counselors is happy to assist you with the following 
-            services without <span className="font-bold">ANY FEES</span> 
-            when you apply to our partner universities.
-          </p>
-          <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold rounded-lg shadow-md hover:from-purple-500 hover:to-blue-500 hover:shadow-lg transform hover:scale-105 transition-all duration-300 ease-in-out uppercase">
+          <div className="bg-[#2A2A2A] text-white p-6 rounded-lg border-2 border-gray-700 max-w-4xl mx-auto mt-12">
+            <h1 className="text-3xl font-bold text-center mb-4">
+              IQRA International Education Consultancy
+            </h1>
+            <p className="text-md mb-4">
+              ...provides step-by-step guidance from application to enrolment to
+              make dreams of higher study abroad a reality for genuine students.
+              Book your{" "}
+              <span className="font-bold text-yellow-400">
+                FREE Consultation
+              </span>{" "}
+              with us now to find out why more than five thousand students
+              consider us the best education consultancy in Bangladesh.
+            </p>
+            <p className="text-md">
+              Our team of expert counselors is happy to assist you with the
+              following services without{" "}
+              <span className="font-bold text-yellow-400">ANY FEES</span>
+              when you apply to our partner universities.
+            </p>
+          </div>
+
+          <button className="px-6 py-3 bg-gradient-to-r from-gray-800 to-green-300 text-white font-semibold rounded-lg shadow-md hover:from-purple-200 hover:to-blue-300 hover:shadow-lg transform hover:scale-105 transition-all duration-600 ease-in-out uppercase">
             Get Started
           </button>
         </div>
