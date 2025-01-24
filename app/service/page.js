933743b4e94475fba91775ba303f7ea9dcd90676
Rasel Miah <rasel6041@gmail.com>
2025-01-24 17:26:11 +0100

    image

--- app/service/page.js
+++ app/service/page.js
@@ -8,73 +8,73 @@ export default function Services() {
       heading: "Career Counseling",
       details:
         "Psychometric testing to find your Field of Interest and Skill Set to help you decide on the right Career Path.",
-      image: "/1.png",
+      image: "/serv/1.png",
     },
     {
       heading: "Education Counseling",
       details:
         "Our Counselors receive extensive training to provide proper guidance in choosing the right field and level of study based on your Future Aspirations. Most of our counselors have been international students themselves and all have been trained abroad.",
-      image: "/2.png",
+      image: "/serv/3.jpg",
     },
     {
       heading: "University & Course Selection",
       details:
         "Guidance in picking the right University based on academic qualification, budget, family circumstances, immigration plans, etc.",
-      image: "/3.png",
+      image: "/serv/2.jpg",
     },
     {
       heading: "Application Placement",
       details:
         "Our dedicated Applications Team makes sure applications meet entry requirements, are placed properly, and that offers are received in a timely manner. We are a registered UCAS center and provide support with OUAC and Common App.",
-      image: "/4.png",
+      image: "/serv/7.jpg",
     },
     {
       heading: "Guidance on SOP/Personal Statement/Scholarship Essay Writing",
       details:
         "Take advantage of our Specialized Essay Support Team to help you come up with the best version for your essay with correct guidelines and constant feedback.",
-      image: "/5.png",
+      image: "/serv/6.jpg",
     },
     {
       heading: "English Proficiency Tests",
       details:
         "We are a certified center of Registration for IELTS, PTE, Duo Lingo, SELT, and OIETC.",
-      image: "/6.png",
+      image: "/serv/3.jpg",
     },
     {
       heading: "Scholarships",
       details:
         "We try our best to connect you with available scholarships to make study abroad more affordable. Many of our students receive scholarships for higher studies.",
-      image: "/7.png",
+      image: "/serv/4.jpg",
     },
     {
       heading: "Pre-Visa Assistance",
       details:
         "We conduct sessions and mocks to prepare students for Credibility and Pre-CAS interviews.",
-      image: "/8.png",
+      image: "/serv/2.png",
     },
     {
       heading: "Expert Visa Services",
       details:
         "Our visa success is noteworthy as we have a dedicated Visa Team with vast experience and expertise.",
-      image: "/9.png",
+      image: "/serv/5.jpg",
     },
     {
       heading: "Pre & Post Departure Support",
       details:
         "We conduct regular briefings to cover all areas in preparing students for a smooth transition to a new country.",
-      image: "/10.png",
+      image: "/serv/10.png",
     },
     {
       heading: "English Test Prep",
       details:
         "Let us help you cross the hurdle to get the required score on your English Test.",
-      image: "/11.png",
+      image: "/serv/8.webp",
     },
     {
       heading: "Live Support",
       details:
         "Have a question? Reach out to our Team on Social Media, Instant Messaging, and more.",
-      image: "/12.png",
+      image: "/serv/12.jpg",
     },
   ];
 
